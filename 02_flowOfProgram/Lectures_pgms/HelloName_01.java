2b285ac255f825566e19b68ce89f94b8c64153f9
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-27 07:41:56 +0000

    changed directory of files.

---
+++ 02_flowOfProgram/Lectures_pgms/HelloName_01.java
@@ -0,0 +1,12 @@
+package Lectures_pgms;
+import java.util.Scanner;
+
+class name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is: "+name);
+
+    }
+}
---
+++ 02_flowOfProgram/Lectures_pgms/PrimeNum_03.java
@@ -0,0 +1,23 @@
+package Lectures_pgms;
+import java.util.Scanner;
+class PrimeNum{
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        int c = 2;
+        boolean isPrime = true;
+        while(c * c <= num){
+            if( num % c == 0){
+                isPrime = false;
+                break;
+            }
+            c++;
+        }
+        if (isPrime) {
+            System.out.println("Number is prime.");
+        } else{
+            System.out.println("Number is not prime." );
+        }
+    }
+}
